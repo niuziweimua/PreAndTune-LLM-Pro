56373a41b477cc3aa1b8d0726da6074129ebfdb8
niuziweimua <133206567+niuziweimua@users.noreply.github.com>
2022-07-21 09:17:17 +0000

    staging: android: lmk: skip if killed by lmk

--- llm_pretrain/pretrain.py
+++ llm_pretrain/pretrain.py
@@ -9,4 +9,97 @@
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permis
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import copy
+import logging
+import os
+from dataclasses import dataclass, field
+from typing import Dict, Optional, Sequence
+import wandb
+
+import torch
+import transformers
+import utils
+from torch.utils.data import Dataset
+from transformers import Trainer
+from generate_pretrain_data import RawPretrainDataset
+
+
+IGNORE_INDEX = -100
+DEFAULT_PAD_TOKEN = "[PAD]"
+DEFAULT_EOS_TOKEN = "</s>"
+DEFAULT_BOS_TOKEN = "<s>"
+DEFAULT_UNK_TOKEN = "<unk>"
+
+
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
+
+
+
+@dataclass
+class DataArguments:
+    data_path: str = field(default=None, metadata={"help": "Path to the training data."})
+
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    optim: str = field(default="adamw_torch")
+    model_max_length: int = field(
+        default=512,
+        metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
+    )
+
+
+def smart_tokenizer_and_embedding_resize(
+    special_tokens_dict: Dict,
+    tokenizer: transformers.PreTrainedTokenizer,
+    model: transformers.PreTrainedModel,
+):
+    """Resize tokenizer and embedding.
+
+    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
+    """
+    num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+    model.resize_token_embeddings(len(tokenizer))
+
+    if num_new_tokens > 0:
+        input_embeddings = model.get_input_embeddings().weight.data
+        output_embeddings = model.get_output_embeddings().weight.data
+
+        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+
+        input_embeddings[-num_new_tokens:] = input_embeddings_avg
+        output_embeddings[-num_new_tokens:] = output_embeddings_avg
+
+
+def _tokenize_fn(strings: Sequence[str], tokenizer: transformers.PreTrainedTokenizer) -> Dict:
+    """Tokenize a list of strings."""
+    tokenized_list = [
+        tokenizer(
+            text,
+            return_tensors="pt",
+            padding="longest",
+            # max_length=tokenizer.model_max_length,
+            truncation=False,
+        )
+        for text in strings
+    ]
+    raw_input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
+    # split input_ids with model_max_length
+    input_ids = []
+    labels = []
+    max_length = tokenizer.model_max_length
+    for instance_ids in raw_input_ids:
+        for i in range(0, len(instance_ids), max_length):
+            input_ids.append(instance_ids[i : i + max_length])
+            labels.append(instance_ids[i : i + max_length])
+            if len(instance_ids[i : i + max_length]) < max_length:
+                print('Warning: len(instance_ids[i : i + max_length]) < max_length')
+                logging.warning(f"len(instance_
