1f5d78c80bdb314160f87706c7cf646c85635da2
niuziweimua <133206567+niuziweimua@users.noreply.github.com>
2023-05-28 17:20:02 +0000

    delete space

--- llm_sft/evaluation/inference_single.py
+++ llm_sft/evaluation/inference_single.py
@@ -43,3 +43,29 @@ if __name__ == '__main__':
             tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True, trust_remote_code=True)
         else:
             tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True, trust_remote_code=True)
+            model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", trust_remote_code=True)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(model_path)
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    generator = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=device)
+
+    prompt_no_input = PROMPT_DICT_ALPACA["prompt_no_input"]
+
+    while True:
+        print('-' * 100)
+        user_input = ''
+        while True:
+            cur_input = input('# user: ')
+            if cur_input == 'q':
+                break
+            user_input += cur_input
+            user_input += '\n'
+        prompt = {
+            "instruction": user_input,
+        }
+        if 'mistral' in model_path.lower():
+            outputs = generator([prompt_no_input.format_map(prompt)], max_new_tokens=200, num_beams=4, pad_token_id=tokenizer.eos_token_id)
+        else:
+            outputs = generator([prompt_no_input.format_map(prompt)], max_new_tokens=200, num_beams=4)
+        print('# bot: \n', outputs[0][0]['generated_text'][len(prompt_no_input.format_map(prompt)):].strip())
